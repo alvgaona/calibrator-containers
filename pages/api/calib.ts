289b2a0a906e6655e75972e76a568ce1b7192250
Alvaro <alvgaona@gmail.com>
2024-08-22 02:22:49 -0300

    fix: update message

--- pages/api/calib.ts
+++ pages/api/calib.ts
@@ -12,9 +12,7 @@ export default function handler(
 ) {
   let mat = cv.matFromArray(2, 3, cv.CV_8UC1, [1, 2, 3, 4, 5, 6]);
 
-  res
-    .status(200)
-    .json({
-      message: `I'm an OpenCV matrix with size ${(mat.rows, mat.cols)}`,
-    });
+  res.status(200).json({
+    message: `I'm an OpenCV matrix with size (${mat.rows}, ${mat.cols})`,
+  });
 }
